29e7bb2ab6acd0cf8582b85f5aaac24485e4652b
Alexander Khitev <alexsanderkhitev@gmail.com>
2022-08-28 20:30:41 +0400

    Initial Commit

---
+++ StateObjectForStackOverflowDemo/StateObjectForStackOverflowDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateObjectForStackOverflowDemoApp.swift
+//  StateObjectForStackOverflowDemo
+//
+//  Created by Alexander Khitev on 8/28/22.
+//
+
+import SwiftUI
+
+@main
+struct StateObjectForStackOverflowDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
